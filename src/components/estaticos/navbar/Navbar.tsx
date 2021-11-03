9ac02f399673c3d64b90a3265b16dc9f341665f4
Lucas Capelotto da silva <lcapelotto@gmail.com>
2021-11-02 23:52:52 -0300

    1.0

---
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import './Navbar.css'
+function Navbar() {
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box className='cursor'>
+                        <Typography variant="h5" color="inherit">
+                            BlogPessoal
+                        </Typography>
+                    </Box>
+                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+
+                        <Box display="flex" justifyContent="start">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    home
+                                </Typography>
+                            </Box>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    postagens
+                                </Typography>
+                            </Box>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    temas
+                                </Typography>
+                            </Box>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    cadastrar tema
+                                </Typography>
+                            </Box>
+                            <Link to='/login' className='text-decorator-none'>
+                                <Box mx={1} className='cursor'>
+                                    <Typography variant="h6" color="inherit">
+                                        logout
+                                    </Typography>
+                                </Box>
+                            </Link>
+
+                        </Box>
+
+                    </Box>
+
+
+                </Toolbar>
+            </AppBar>
+
+        </>
+    )
+}
+
+export default Navbar;
